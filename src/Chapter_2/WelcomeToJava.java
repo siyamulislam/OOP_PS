7539c4a1df265eeebcda1ef9bef9e0155b8fb884
Siyamul Islam <53119070+siyamulislam@users.noreply.github.com>
2022-06-25 22:40:01 +0600

    level 1 problem

---
+++ src/Chapter_2/WelcomeToJava.java
@@ -0,0 +1,18 @@
+package Chapter_2;
+/**
+ * Level 1
+ * 17.Write a program that displays a frame window 800 pixels wide and 600 pixels high.  Input the user’s first and last name as two separate strings. Then display a
+frame window with its title set to <last>, <first>, where <last> and <first> are
+the input values. For example, if the input values are Johann and Strauss,
+then the title would be Strauss, Johann.
+*  @author Siam_PC
+ */
+import javax.swing.*;
+public class WelcomeToJava {
+   public static void main(String arg []){
+       JFrame window=new JFrame();
+       window.setSize(800, 600);
+       window.setTitle("Welcome to Java");
+       window.setVisible(true);
+   }   
+}
