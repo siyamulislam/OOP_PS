2e16497723477e1c5d1a0fe7014ffc6c2705fd7f
Siyamul Islam <53119070+siyamulislam@users.noreply.github.com>
2022-06-26 23:06:20 +0600

    String joint and substring match ex 19

---
+++ src/Chapter_2/Ex19inputShowInitial.java
@@ -0,0 +1,27 @@
+/**
+ * Level 1 19. Input the user’s first, middle, and last name as three separate strings and display
+ * the name in the order of the first name, the middle initial, and the last name. Include the
+ * period after the middle initial. If the input strings are Wolfgang, Amadeus, and Mozart, for
+ * example, then the output would be Wolfgang A. Mozart. Use the console window for output.
+ *
+ * @author Siam_PC
+ */
+package Chapter_2;
+import java.util.Scanner;
+public class Ex19inputShowInitial {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String firstName, middleName, lastName;
+
+        System.out.println("Enter your First Name"); // Input First Name
+        firstName = sc.next();
+        System.out.println("Enter your Middle Name"); // Input Middle Name
+        middleName = sc.next();
+        System.out.println("Enter your Last Name"); // Input Last Name
+        lastName = sc.next();
+
+        System.out.println(firstName + " " + middleName.substring(0, 1) + "." + " " + lastName);
+
+    }
+}
