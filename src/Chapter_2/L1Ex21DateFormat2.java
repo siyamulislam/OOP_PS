bd6a80f264274800da8789a410bf910384020533
Siyamul Islam <53119070+siyamulislam@users.noreply.github.com>
2022-06-28 23:27:56 +0600

    date format 2

---
+++ src/Chapter_2/L1Ex21DateFormat2.java
@@ -0,0 +1,20 @@
+/**
+ * Level 1 20. Write a program to display today’s date in this format: 10 December 2008.
+ * Use the console window for output. 
+ *
+ * @author Siam_PC
+ */
+package Chapter_2;
+
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Date;
+
+public class L1Ex21DateFormat2 {
+
+    public static void main(String[] args) {
+        Date today=new Date();
+        SimpleDateFormat sdf1=new SimpleDateFormat("EEE MMMM dd, YYYY");
+        System.out.println(sdf1.format(today));
+    }
+}
