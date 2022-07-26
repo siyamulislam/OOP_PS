a9cd39c405e71be035cea94f5111a9662fc3997a
Siyamul Islam <53119070+siyamulislam@users.noreply.github.com>
2022-07-26 22:57:39 +0600

    prime number rf

--- src/Chapter_6/L1Ex7PrimeNumber.java
+++ src/Chapter_6/L1Ex7PrimeNumber.java
@@ -1,13 +1,14 @@
 /**
- * Level 1 ex 06. Write a program to print out the numbers 10 through 49 in the following
- * manner:
- * 10 11 12 13 14 15 16 17 18 19
- * 20 21 22 23 24 25 26 27 28 29
- * 30 31 32 33 34 35 36 37 38 39
- * 40 41 42 43 44 45 46 47 48 49
+ * Level 1 ex 06. A prime number is an integer greater than 1 and divisible by only itself and 1.
+ * The first seven prime numbers are 2, 3, 5, 7, 11, 13, and 17.Write a method that returns true
+ * if its parameter is a prime number. Using this method, write a program that repeatedly asks the
+ * user for input and displays  Prime if the input is a prime number and Not Prime, otherwise.
+ * Stop the repetition when the input is a negative number.
  *
  */
 package Chapter_6;
+
+import java.util.Scanner;
 
 /**
  *
@@ -16,13 +17,32 @@ package Chapter_6;
 public class L1Ex7PrimeNumber {
 
     public static void main(String[] args) {
-        for (int i = 10, j = 0; i <= 49; i++, j++) {
-            if (j == 10) {
-                System.out.println();
-                j = 0;
+        Scanner scanner = new Scanner(System.in);
+        for (;;) {
+            int number;
+            System.out.print("Enter a number: ");
+            number = scanner.nextInt();
+            if (number < 0) {
+                break;
             }
-            System.out.print(i + " ");
+            if (CheckIfPrime(number)) {
+                System.out.println("Prime");
+            } else {
+                System.out.println("Not Prime");
+            }
         }
-        System.out.println();
+    }
+
+    private static boolean CheckIfPrime(int num) {
+        boolean isPrime = true;
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) {
+                isPrime = false;
+                break;
+            } else {
+                isPrime = true;
+            }
+        }
+        return isPrime;
     }
 }
