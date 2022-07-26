ddec64d7d7f693d34d6d6d425179fec19e40a818
Siyamul Islam <53119070+siyamulislam@users.noreply.github.com>
2022-07-27 00:01:34 +0600

    degit of number

--- src/Chapter_6/L2Ex18degitCount.java
+++ src/Chapter_6/L2Ex18degitCount.java
@@ -1,48 +1,44 @@
 /**
- * Level 1 ex 06. A prime number is an integer greater than 1 and divisible by only itself and 1.
- * The first seven prime numbers are 2, 3, 5, 7, 11, 13, and 17.Write a method that returns true
- * if its parameter is a prime number. Using this method, write a program that repeatedly asks the
- * user for input and displays  Prime if the input is a prime number and Not Prime, otherwise.
- * Stop the repetition when the input is a negative number.
- *
+ * Level 2 ex 18. Write a method that returns the number of digits in an integer argument; 
+ * for example, 23,498 has five digits. Using this method, write a program that repeatedly 
+ * asks for input and displays the number of digits the input integer has. Stop the repetition 
+ * when the input value is negative.
  */
 package Chapter_6;
 
+import java.util.Date;
 import java.util.Scanner;
 
 /**
  *
  * @author Siam_PC
  */
-public class L1Ex7PrimeNumber {
+public class L2Ex18degitCount {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        for (;;) {
-            int number;
-            System.out.print("Enter a number: ");
-            number = scanner.nextInt();
-            if (number < 0) {
+                Scanner scanner = new Scanner(System.in);
+        for(;;)
+        {
+            int n, noOfDigits = 0;
+            System.out.print("Enter an Integer: ");
+            n = scanner.nextInt();
+            if(n < 0)
+            {
                 break;
             }
-            if (CheckIfPrime(number)) {
-                System.out.println("Prime");
-            } else {
-                System.out.println("Not Prime");
-            }
+            noOfDigits = GetDigitOfAIntefer(n);
+            System.out.println("No of Digits in the Integer: "+noOfDigits);
         }
     }
-
-    private static boolean CheckIfPrime(int num) {
-        boolean isPrime = true;
-        for (int i = 2; i * i <= num; i++) {
-            if (num % i == 0) {
-                isPrime = false;
-                break;
-            } else {
-                isPrime = true;
-            }
+    
+    private static int GetDigitOfAIntefer(int num)
+    {
+        int count = 0;
+        while(num > 0)
+        {
+            num = num/10;
+            count ++;
         }
-        return isPrime;
+        return count;
     }
 }
