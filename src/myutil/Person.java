86e52e905b9cdd5730dd8383316af9904dbad833
siyamulislam <siyamulislam1@gmail.com>
2022-08-02 17:27:00 +0600

    myutil package

---
+++ src/myutil/Person.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myutil;
+
+/**
+ *
+ * @author Siam
+ */
+public class Person {
+
+    private String name;
+
+    public Person() {
+        name = "Unknown";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
