f0fe7132415f97df6094076a3fe3b48c770489bb
siyamulislam <siyamulislam1@gmail.com>
2022-08-02 18:22:14 +0600

    MealCardTest

--- src/Chapter_7/L1Ex16MealCardTest.java
+++ src/Chapter_7/L1Ex16MealCardTest.java
@@ -1,14 +1,17 @@
 /**
- * Level 1 ex 14.  Write a program that tests the Person class defined in Exercise 13. Place this
-test program outside of the myutil package and make sure you can call the
-constructor and the methods of the Person class correctly.
-
-
+ * Level 1 ex 16. Write a program that tests the meal card class defined in
+ * Exercise 15. Define this test program outside the myutil package. Create one
+ * or two meal card objects in the test program and verify that all methods
+ * defined in the meal card class operate correctly.
+ *
+ *
+ *
  *
  */
 package Chapter_7;
 
-import myutil.Person;
+import java.util.Scanner;
+import myutil.MealCard;
 
 /**
  *
@@ -17,14 +20,19 @@ import myutil.Person;
 public class L1Ex16MealCardTest {
 
     public static void main(String[] args) {
-        Person person = new Person();
+        MealCard mealCard = new MealCard();
 
-        System.out.println(person.getName()); 
-        
-        person.setName("Siam");
-        System.out.println(person.getName());      
+        System.out.println("Initial Balance: " + mealCard.getBalance());
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the Bought amount:");
+        int cost = sc.nextInt();
+        mealCard.deductBalance(cost);
+        System.out.println("Cutrrent Balance: " + mealCard.getBalance());
+
+        System.out.print("Enter The purchase Point amount:");
+        int buyAmt = sc.nextInt();
+        mealCard.purchasePoints(buyAmt);
+        System.out.println("Current Balance: " + mealCard.getBalance());
     }
 }
-
-	
-	
