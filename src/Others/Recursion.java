22787a9f8cc1b8f7214b3ab33dbb7e48ffb88f4f
siyamulislam <siyamulislam1@gmail.com>
2022-08-29 22:12:17 +0600

    Recursion

---
+++ src/Others/Recursion.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Others;
+
+/**
+ *
+ * @author Siam
+ */
+public class Recursion {
+
+    public static void main(String[] args) {
+        int result = sum(100);
+        System.out.println(result);
+    }
+
+    public static int sum(int k) {
+        if (k > 0) {
+            return k + sum(k - 1);
+        } else {
+            return 0;
+        }
+
+    }
+
+}
