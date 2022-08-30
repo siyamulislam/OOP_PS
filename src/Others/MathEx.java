1fcf244384239459db360d10f5818aa7d887be31
siyamulislam <siyamulislam1@gmail.com>
2022-08-30 18:23:27 +0600

    math pack

---
+++ src/Others/MathEx.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Others;
+
+import java.awt.BorderLayout;
+
+/**
+ *
+ * @author Siam
+ */
+public class MathEx {
+    
+    public static void main(String[] args) {
+        int x=20;
+        System.out.println(Math.exp(x)); //exp()
+        System.out.println(Math.round(12.5)); //round()
+        System.out.println(Math.floor(12.9)); //floor()
+        System.out.println(Math.ceil(12.1)); //ceil()
+        System.out.println(Math.pow(12,5)); //ceil()
+        System.out.println(Math.sin(90)); //ceil()
+        System.out.println(Math.cos(90)); //ceil()
+        System.out.println(Math.sqrt(12.1)); //ceil()
+        System.out.println(Math.log(12.1)); //ceil()
+    }
+    
+}
