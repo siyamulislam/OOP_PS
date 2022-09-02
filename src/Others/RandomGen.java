32a2d3e79c5d8d18f40202ba32d4df27a9cbd4ab
siyamulislam <siyamulislam1@gmail.com>
2022-09-02 19:51:56 +0600

    ramdonnum

---
+++ src/Others/RandomGen.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Others;
+
+import java.util.Random;
+
+/**
+ *
+ * @author Siam
+ */
+public class RandomGen {
+    public static void main(String[] args) {
+        Random random=new Random();
+        int number0 =random.nextInt(3);
+//        int number1 =random.nextInt((4-2+1)+2); //not working
+         int number1 =(int)Math.floor(5+(Math.random()*(10-5)));
+        System.out.println(number1);
+        System.out.println( 5 + (Math.random() * (10 - 5)) );
+    }
+          
+}
